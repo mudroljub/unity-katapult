ec418711c7614b811c22614dfdcdfee052dcdd87
Damjan Pavlica <mudroljub@gmail.com>
2020-04-01 00:08:23 +0100

    InitWall

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,21 @@ public class GameManager : MonoBehaviour
     public Catapult catapult;
     public Cube cube;
 
-    private void Start()
+    void InitRow(int y)
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(cube, new Vector3(140, y, -10 + i * 2), Quaternion.identity);
+        }
+    }
+
+    void InitWall()
+    {
+        for (int j = 0; j < 7; j++) InitRow(1 + j * 2);
+    }
+
+    void Start()
+    {
+        InitWall();
     }
 }
