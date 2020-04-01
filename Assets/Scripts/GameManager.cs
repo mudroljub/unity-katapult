41d9f7bf4845f648facdafa14968ff67e2a8f41b
Damjan Pavlica <mudroljub@gmail.com>
2020-04-01 18:45:25 +0200

    rezultat

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,4 @@ public class GameManager : MonoBehaviour
 {
     public Camera mainCam;
     public Camera uiCam;
-    public Catapult catapult;
-    public Wall wall;
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.End))
-        {
-            byte total = wall.CheckCollapsed();
-            print("Ukupno je oboreno " + total);
-            print("Ukupno je ispaljeno " + catapult.ballsLaunched);
-        }
-    }
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    public Catapult catapult;
+    public Wall wall;
+
+    void Update()
+    {
+        byte collapsed = wall.CheckCollapsed();
+        scoreText.text = "Ispaljeno: " + catapult.ballsLaunched
+            + "\nOboreno: " + collapsed
+            + "\nREZULTAT: " + (collapsed - catapult.ballsLaunched);
+    }
+}
